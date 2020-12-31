5ba3c6f8d5a6b7b94d27ca2d4a7e064d5ee16522
Rohan Dutt <duttrohan0302@gmail.com>
2020-12-31 21:18:52 +0530

    init: initialize routes folder and imported to server.js

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+// const routeName = require('./routeLocation)
+
+// router.use('route',routeName)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 5000;
 
@@ -7,12 +8,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// routes
-app.get('/', (req, res) => {
-	res.send('Hello from MERN');
-});
 
-// Bootstrap server
+const routes = require('./routes');
+
+
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}.`);
 });
