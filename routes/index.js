56b464caf5108796cd5de47ef986f093f6cb2ce9
Rohan Dutt <duttrohan0302@gmail.com>
2020-12-31 22:32:51 +0530

    init: heroku init

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
+const path = require('path')
 // const routeName = require('./routeLocation)
 
 router.get('/hello',async (req,res,next)=> {
-
     res.status(200).json({message:"The frontend is connected to backend"})
 })
 // router.use('route',routeName)
+
+if(process.env.NODE_ENV ==="production"){
+    router.use(function(req, res) {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+}
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 require('dotenv').config()
 const app = express();
+const path = require('path')
+
+
+
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 5000;
@@ -12,7 +16,9 @@ require('./models');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+if(process.env.NODE_ENV ==="production"){
+	app.use(express.static(path.join(__dirname, 'client/build')));
+}
 app.use(routes)
 
 app.listen(PORT, () => {
