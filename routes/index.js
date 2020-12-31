d17762f714a62066022c64d6fc7496b2d9b0922d
Rohan Dutt <duttrohan0302@gmail.com>
2020-12-31 21:29:01 +0530

    init: temp route added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 // const routeName = require('./routeLocation)
 
+router.get('/hello',async (req,res,next)=> {
+
+    res.status(200).json({message:"The frontend is connected to backend"})
+})
 // router.use('route',routeName)
 
 module.exports = router;
