fa1e94488a21b212d8a3286c55fe8c8453dae51f
Rohan Dutt <duttrohan0302@gmail.com>
2021-01-03 01:02:35 +0530

    feat: temp app services

---
+++ services/tempApp.js
@@ -0,0 +1,58 @@
+
+exports.findAll = async function (Model) {
+    try {
+        const data = await Model.find({});
+
+        return data;
+    } catch (e) {
+        return e;
+    }
+}
+
+
+exports.findOne = async function (Model, findBy, value) {
+
+    try {
+        const obj = {}
+        obj[findBy] = value
+        console.log(obj)
+        const data = await Model.findOne({[findBy]:value})
+
+        return data;
+
+    } catch (e) {
+        return e;
+    }
+
+}
+
+exports.create = async function (Model, object) {
+    try {
+        const data = await Model.create(object)
+        return data;
+
+    } catch (e) {
+        return e;
+    }
+}
+
+exports.update = async function (Model, object, findBy, value) {
+    try {
+        console.log("here")
+        const data = await Model.findOneAndUpdate({ [findBy]: value }, object, { new: true, omitUndefined: true })
+        return data;
+
+    } catch (e) {
+        return e;
+    }
+}
+
+exports.delete = async function (Model, findBy, value) {
+    try {
+        const data = await Model.findOneAndDelete({[findBy]:value})
+        return data;
+
+    } catch (e) {
+        return e;
+    }
+}
