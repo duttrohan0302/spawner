bc3a5dc73a8d6e839ff36b9195a1e5af2dff31e1
Rohan Dutt <duttrohan0302@gmail.com>
2021-01-03 00:59:16 +0530

    feat: route change middleware

--- routes/app.js
+++ routes/app.js
@@ -1,18 +1,19 @@
 const router = require('express').Router();
 
 //Import controller
+const AppMiddleware = require('../middlewares/schemaMiddleware')
 const AppController = require('../controllers/app')
 
 
 
 router.post('/app',AppController.create);
 
-router.get('/myapp/:appSlug/:model/:number',AppController.schemaMiddleware, AppController.get)
+router.get('/myapp/:appSlug/:model/:number',AppMiddleware.schemaMiddleware, AppController.get)
 
-router.post('/myapp/:appSlug/:model',AppController.schemaMiddleware, AppController.post)
+router.post('/myapp/:appSlug/:model',AppMiddleware.schemaMiddleware, AppController.post)
 
-router.patch('/myapp/:appSlug/:model/',AppController.schemaMiddleware, AppController.patch)
+router.patch('/myapp/:appSlug/:model/',AppMiddleware.schemaMiddleware, AppController.patch)
 
-router.delete('/myapp/:appSlug/:model/',AppController.schemaMiddleware, AppController.delete)
+router.delete('/myapp/:appSlug/:model/',AppMiddleware.schemaMiddleware, AppController.delete)
 
 module.exports = router;
